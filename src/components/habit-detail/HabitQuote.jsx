ef4033053ebfbecc51dc5a56a4dd22651b530505
Deepak Jain <deepak.pce1992@gmail.com>
2023-08-13 12:19:49 +0530

    implemented error handling for quote api

--- src/components/habit-detail/HabitQuote.jsx
+++ src/components/habit-detail/HabitQuote.jsx
@@ -2,21 +2,27 @@ import classes from "./HabitQuote.module.css";
 
 import { useEffect, useState } from "react";
 
+const QUOTE = {
+  text: "In the long run, consistency beats intensity.",
+  author: "Deepak Jain",
+};
 const HabitQuote = () => {
-  const [quote, setQuote] = useState({
-    text: "In the long run, consistency beats intensity.",
-    author: "Deepak Jain",
-  });
+  const [quote, setQuote] = useState(QUOTE);
 
   useEffect(() => {
     // get quote from api
     const getQuoteFromAPi = async () => {
-      const response = await fetch(
-        "https://www.jcquotes.com/api/quotes/random"
-      );
-      const data = await response.json();
-      // console.log(data);
-      setQuote({ text: data.text, author: "James Clear" });
+      try {
+        const response = await fetch(
+          "https://www.jcquotes.com/api/quotes/random"
+        );
+        const data = await response.json();
+        // console.log(data);
+        setQuote({ text: data.text, author: "James Clear" });
+      } catch (e) {
+        console.log("💣 👾 Error fetching quote from API");
+        setQuote(QUOTE);
+      }
     };
 
     getQuoteFromAPi();
