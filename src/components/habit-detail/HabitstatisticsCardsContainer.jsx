3de99d088b483578166b4bb311aa7f002fe482cd
Deepak Kumar Jain <deepak.pce1992@gmail.com>
2023-09-09 11:05:52 +0000

    update habit card statistics

--- src/components/habit-detail/HabitstatisticsCardsContainer.jsx
+++ src/components/habit-detail/HabitstatisticsCardsContainer.jsx
@@ -4,10 +4,10 @@ import classes from "./HabitstatisticsCardsContainer.module.css";
 const HabitstatisticsCardsContainer = () => {
     return (
         <div className={classes['card-container']}>
-            <HabitstatisticsCard type="complete" />
-            <HabitstatisticsCard type="fail" />
-            <HabitstatisticsCard type="skip" />
-            <HabitstatisticsCard type="total" />
+            <HabitstatisticsCard cardType="done" />
+            <HabitstatisticsCard cardType="fail" />
+            <HabitstatisticsCard cardType="skip" />
+            <HabitstatisticsCard cardType="total" />
         </div>
     )
 }
